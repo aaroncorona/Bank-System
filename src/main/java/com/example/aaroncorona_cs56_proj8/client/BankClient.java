b334324b0b376529f0aef01118c0976525bb890b
aaroncorona <amc1140@berkeley.edu>
2022-11-25 15:06:09 -0800

    Simplifying client constructor logic

--- src/main/java/com/example/aaroncorona_cs56_proj8/client/BankClient.java
+++ src/main/java/com/example/aaroncorona_cs56_proj8/client/BankClient.java
@@ -1,13 +1,4 @@
-/***************************************************************************
- Author: Aaron Corona
- Date: Nov 25th, 2022
- CS56 Project #8 - Bank System
- ***************************************************************************/
-
 package com.example.aaroncorona_cs56_proj8.client;
-
-import com.example.aaroncorona_cs56_proj8.proxy.Bank;
-import com.example.aaroncorona_cs56_proj8.proxy.BankProxy;
 
 // Driver to launch the Client GUI
 public class BankClient {
